a3f9686ee35e5259c547cabcfe6ca02369b791d3
kolokolkkm <klimenkovknst@gmail.com>
2026-07-22 12:33:10 +0500

    Delete comments

--- Score.cs
+++ Score.cs
@@ -6,12 +6,12 @@ public partial class Score : Node
 {
 	[Signal] public delegate void ValueChangedEventHandler(int newValue);
 
-    [Export] public int maxValue = int.MaxValue; //РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РѕС‡РєРѕРІ, РЅР°СЃС‚СЂР°РёРІР°РµС‚СЃСЏ РІ СЂРµРґР°РєС‚РѕСЂРµ Godot
-	[Export] public int minValue = 0; //РњРёРЅРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РѕС‡РєРѕРІ, РЅР°СЃС‚СЂР°РёРІР°РµС‚СЃСЏ РІ СЂРµРґР°РєС‚РѕСЂРµ Godot
-	[Export] public int startValue = 0; //РЎС‚Р°СЂС‚РѕРІРѕРµ Р·РЅР°С‡РµРЅРёРµ РѕС‡РєРѕРІ, РЅР°СЃС‚СЂР°РёРІР°РµС‚СЃСЏ РІ СЂРµРґР°РєС‚РѕСЂРµ Godot
+    [Export] public int maxValue = int.MaxValue;
+	[Export] public int minValue = 0;
+	[Export] public int startValue = 0;
 
-	public int CurrentValue { get; set; } //РўРµРєСѓС‰РµРµ Р·РЅР°С‡РµРЅРёРµ РѕС‡РєРѕРІ, РґРѕСЃС‚СѓРїРЅРѕ РґР»СЏ С‡С‚РµРЅРёСЏ Рё Р·Р°РїРёСЃРё
-	public int HighScore { get; set; } //РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РѕС‡РєРѕРІ, РґРѕСЃС‚СѓРїРЅРѕ РґР»СЏ С‡С‚РµРЅРёСЏ Рё Р·Р°РїРёСЃРё
+	public int CurrentValue { get; set; }
+	public int HighScore { get; set; }
 
 	public override void _Ready()
 	{
@@ -24,21 +24,18 @@ public partial class Score : Node
         HighScore = startValue;
 	}
 
-	//РњРµС‚РѕРґ РёР·РјРµРЅРµРЅРёСЋ СЃС‡РµС‚С‡РёРєР° РѕС‡РєРѕРІ
 	public void Modify(int amount)
 	{
-        CurrentValue = Mathf.Clamp(CurrentValue + amount, minValue, maxValue); //РџСЂРёСЃРІР°РёРІР°РЅРёРµ Р·РЅР°С‡РµРЅРёРµ СЃ СѓС‡С‘С‚РѕРј РѕРіСЂР°РЅРёС‡РµРЅРёР№ minValue Рё maxValuРµ,
-																			   //РµСЃР»Рё С‚РµРєСѓС‰РµРµ Р·РЅР°С‡РµРЅРёРµ РІС‹С…РѕРґРёС‚ Р·Р° РїСЂРµРґРµР»С‹,
-																			   //С‚Рѕ РѕРЅРѕ Р±СѓРґРµС‚ СѓСЃС‚Р°РЅРѕРІР»РµРЅРѕ РІ Р±Р»РёР¶Р°Р№С€РµРµ РѕРіСЂР°РЅРёС‡РµРЅРёРµ
+        CurrentValue = Mathf.Clamp(CurrentValue + amount, minValue, maxValue);
 
 		if (CurrentValue > HighScore)
 		{
-            HighScore = CurrentValue; //РћР±РЅРѕРІР»СЏРµС‚СЃСЏ СЂРµРєРѕСЂРґ, РµСЃР»Рё С‚РµРєСѓС‰РµРµ Р·РЅР°С‡РµРЅРёРµ РѕС‡РєРѕРІ РїСЂРµРІС‹С€Р°РµС‚ СЂРµРєРѕСЂРґРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ
+            HighScore = CurrentValue;
 		}
 
         EmitSignal(SignalName.ValueChanged, CurrentValue);
     }
 
-	public void ResetCurrentValue() => CurrentValue = startValue; //РЎР±СЂРѕСЃ С‚РµРєСѓС‰РµРіРѕ Р·РЅР°С‡РµРЅРёСЏ РѕС‡РєРѕРІ Рє СЃС‚Р°СЂС‚РѕРІРѕРјСѓ Р·R
-	public void ResetHighScore() => HighScore = CurrentValue; //РЎР±СЂРѕСЃ СЂРµРєРѕСЂРґНё Р·R
+	public void ResetCurrentValue() => CurrentValue = startValue;
+	public void ResetHighScore() => HighScore = CurrentValue;
 }
--- Streak.cs
+++ Streak.cs
@@ -6,12 +6,12 @@ public partial class Streak : Node
 {
     [Signal] public delegate void ValueChangedEventHandler(int newValue);
 
-    [Export] public int maxValue = int.MaxValue; //РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ СЃРµСЂРёРё, РЅР°СЃС‚СЂР°РёРІР°РµС‚СЃСЏ РІ СЂРµРґР°РєС‚РѕСЂРµ Godot
-	[Export] public int minValue = 0; //РњРёРЅРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ СЃРµСЂРёРё, РЅР°СЃС‚СЂР°РёРІР°РµС‚СЃСЏ РІ СЂРµРґР°РєС‚РѕСЂРµ Godot
-	[Export] public int startValue = 0; //РЎС‚Р°СЂС‚РѕРІРѕРµ Р·РЅР°С‡РµРЅРёРµ СЃРµСЂРёРё, РЅР°СЃС‚СЂР°РёРІР°РµС‚СЃСЏ РІ СЂРµРґР°РєС‚РѕСЂРµ Godot
+    [Export] public int maxValue = int.MaxValue;
+	[Export] public int minValue = 0;
+	[Export] public int startValue = 0;
 
-	public int CurrentValue { get; set; } //РўРµРєСѓС‰РµРµ Р·РЅР°С‡РµРЅРёРµ СЃРµСЂРёРё, РґРѕСЃС‚СѓРїРЅРѕ РґР»СЏ С‡С‚РµРЅРёСЏ Рё Р·Р°РїРёСЃРё
-	public int MaxStreak { get; set; } //РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ СЃРµСЂРёРё, РґРѕСЃС‚СѓРїРЅРѕ РґР»СЏ С‡С‚РµРЅРёСЏ Рё Р·Р°РїРёСЃРё
+	public int CurrentValue { get; set; }
+	public int MaxStreak { get; set; }
 
 	public override void _Ready()
 	{
@@ -26,17 +26,17 @@ public partial class Streak : Node
 
 	public void Increment()
 	{
-        CurrentValue = Mathf.Clamp(CurrentValue + 1, minValue, maxValue); //РЈРІРµР»РёС‡РµРЅРёРµ СЃРµСЂРёРё РЅР° 1 РІ РїСЂРµРґРµР»Р°С… РѕРіСЂР°РЅРёС‡РµРЅРёР№
+        CurrentValue = Mathf.Clamp(CurrentValue + 1, minValue, maxValue);
 
 		if (CurrentValue > MaxStreak)
 		{
-            MaxStreak = CurrentValue; //РћР±РЅРѕРІР»РµРЅРёРµ СЂРµРєРѕСЂРґР°, РµСЃР»Рё С‚РµРєСѓС‰РµРµ Р·РЅР°С‡РµРЅРёРµ РµРіРѕ РїСЂРµРІС‹С€Р°РµС‚
+            MaxStreak = CurrentValue;
 		}
 
         EmitSignal(SignalName.ValueChanged, CurrentValue);
     }
 
-	public void ResetCurrentValue() => CurrentValue = startValue; //РЎР±СЂРѕСЃ С‚РµРєСѓС‰РµРіРѕ Р·РЅР°С‡РµРЅРёСЏ СЃРµСЂРёРё Рє СЃС‚Р°СЂС‚РѕРІРѕРјСѓ Р·РЅР°С‡РµРЅРёСЋ
+	public void ResetCurrentValue() => CurrentValue = startValue;
 
-	public void ResetMaxStreak() => MaxStreak = CurrentValue; //РЎР±СЂРѕСЃ СЂРµРєРѕСЂРґРЅРѕРіРѕ Р·РЅР°С‡РµРЅРёСЏ СЃРµСЂРёРё Рє С‚РµРєСѓС‰РµРјСѓ Р·РЅР°С‡РµРЅРёСЋ
+	public void ResetMaxStreak() => MaxStreak = CurrentValue;
 }
